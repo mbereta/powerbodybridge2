c47d1008e6e859c5ef28911d2476ef1bf2c88669
Robert Bednarowski <bednarowskir@gmail.com>
2018-06-08 15:35:52 +0200

    SLL-159

--- Model/ResourceModel/Export/OrderEntryRepository.php
+++ Model/ResourceModel/Export/OrderEntryRepository.php
@@ -4,33 +4,41 @@ declare(strict_types=1);
 
 namespace Powerbody\Bridge\Model\ResourceModel\Export;
 
+use \Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry;
+
 class OrderEntryRepository implements OrderEntryRepositoryInterface
 {
-    
+
     /** @var \Powerbody\Bridge\System\Configuration\ConfigurationReaderInterface */
     private $configurationReader;
-    
+
     /** @var \Magento\Sales\Model\OrderFactory */
     private $orderModelFactory;
-    
+
     /** @var \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry\CollectionFactory */
     private $statusCollectionFactory;
-    
+
     /** @var \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry */
     private $resourceModel;
-    
+
+    /** var OrderCollectionFactory */
+    private $orderCollectionFactory;
+
     public function __construct(
         \Powerbody\Bridge\System\Configuration\ConfigurationReaderInterface $configurationReader,
         \Magento\Sales\Model\OrderFactory $orderFactory,
         \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry\CollectionFactory $statusCollectionFactory,
-        \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry $resourceModel
+        \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry $resourceModel,
+        OrderCollectionFactory $orderCollectionFactory
     ) {
         $this->configurationReader = $configurationReader;
         $this->orderModelFactory = $orderFactory;
         $this->statusCollectionFactory = $statusCollectionFactory;
         $this->resourceModel = $resourceModel;
+        $this->orderCollectionFactory = $orderCollectionFactory;
     }
-    
+
     /**
      * @return \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry\Collection
      */
@@ -38,9 +46,9 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
     {
         /** @var \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry\Collection $collection */
         $collection = $this->statusCollectionFactory->create();
-    
+
         $states = $this->configurationReader->getExportOrderStates();
-    
+
         $collection->getSelect()
             ->join(
                 ['order' => $collection->getTable('sales_order')],
@@ -48,7 +56,7 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
                 'status'
             )
             ->where('order.status IN (?)', $states);
-        
+
         return $collection
             ->addFieldToFilter(
                 'main_table.status',
@@ -56,7 +64,24 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
             )
             ->load();
     }
-    
+
+    public function getOrderCollectionToPush() : \Magento\Sales\Model\ResourceModel\Order\Collection
+    {
+        /* @var $orderCollection \Magento\Sales\Model\ResourceModel\Order\Collection */
+        $orderCollection = $this->orderCollectionFactory->create();
+        $orderCollection->addFieldToFilter('main_table.status', ['in' => $this->configurationReader->getUpdateOrderStates()]);
+
+        $orderCollection->getSelect()
+            ->joinLeft(
+                ['order_entry' => $orderCollection->getTable(OrderEntry::TABLE_NAME)],
+                'order_entry.order_id = main_table.entity_id',
+                ''
+            )
+            ->where('order_entry.order_id IS NULL');
+
+        return $orderCollection;
+    }
+
     /**
      * @return \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry\Collection
      */
@@ -64,9 +89,9 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
     {
         /** @var \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntry\Collection $collection */
         $collection = $this->statusCollectionFactory->create();
-    
+
         $states = $this->configurationReader->getUpdateOrderStates();
-    
+
         $collection->getSelect()
             ->join(
                 ['order' => $collection->getTable('sales_order')],
@@ -74,7 +99,7 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
                 'status'
             )
             ->where('order.status IN (?)', $states);
-    
+
         return $collection
             ->addFieldToFilter(
                 'main_table.status',
@@ -82,7 +107,7 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
             )
             ->load();
     }
-    
+
     public function save(\Powerbody\Bridge\Model\Export\OrderEntry $orderTransferStatus)
     {
         try {
@@ -91,5 +116,5 @@ class OrderEntryRepository implements OrderEntryRepositoryInterface
             throw new \Magento\Framework\Exception\CouldNotSaveException($e->getMessage());
         }
     }
-    
+
 }
--- Service/OrderService.php
+++ Service/OrderService.php
@@ -4,58 +4,89 @@ declare(strict_types=1);
 
 namespace Powerbody\Bridge\Service;
 
+use  \Powerbody\Bridge\Model\Export\OrderEntryFactory;
+
 class OrderService implements OrderServiceInterface
 {
-    
+
     /** @var \Powerbody\Bridge\Entity\Order\RepositoryInterface */
     private $entityOrderEntryRepository;
-    
+
     /** @var \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntryRepositoryInterface */
     private $orderEntryRepository;
-    
+
     /** @var \Magento\Sales\Model\OrderRepository */
     private $orderRepository;
-    
+
     /** @var \Magento\Sales\Model\Order\Config */
     private $orderConfig;
-    
+
     /** @var ShipmentService */
     private $shipmentService;
-    
+
+     /** @var OrderEntryFactory */
+    private $exportOrderEntryFactory;
+
     public function __construct(
         \Powerbody\Bridge\Entity\Order\RepositoryInterface $entityOrderEntryRepository,
         \Powerbody\Bridge\Model\ResourceModel\Export\OrderEntryRepositoryInterface $orderEntryRepository,
         \Magento\Sales\Model\OrderRepository $orderRepository,
         \Magento\Sales\Model\Order\Config $orderConfig,
-        ShipmentServiceInterface $shipmentService
+        ShipmentServiceInterface $shipmentService,
+        OrderEntryFactory $exportOrderEntryFactory
     ) {
         $this->entityOrderEntryRepository = $entityOrderEntryRepository;
         $this->orderEntryRepository = $orderEntryRepository;
         $this->orderRepository = $orderRepository;
         $this->orderConfig = $orderConfig;
         $this->shipmentService = $shipmentService;
+        $this->exportOrderEntryFactory = $exportOrderEntryFactory;
     }
-    
+
     public function exportOrderById(int $orderId) : array
     {
         /** @var \Magento\Sales\Model\Order $order */
         $order = $this->orderRepository->get($orderId);
-    
+
         /** @var array $orderData */
         $orderData = $this->prepareOrderData($order);
-    
+
         return $this->entityOrderEntryRepository->createOrder($orderData);
     }
-    
+
+    public function addOrdersToExportOrderEntry()
+    {
+        /* @var $orderCollection \Magento\Sales\Model\ResourceModel\Order\Collection */
+        $orderCollection = $this->orderEntryRepository->getOrderCollectionToPush();
+
+        /* @var $orderModel \Magento\Sales\Model\Order */
+        foreach ($orderCollection as $orderModel) {
+            /* @var $orderEntryModel \Powerbody\Bridge\Model\Export\OrderEntry */
+            $orderEntryModel = $this->exportOrderEntryFactory->create();
+
+            $orderEntryModel->setData([
+                'id' => null,
+                'order_id' => $orderModel->getData('entity_id'),
+                'status' => \Powerbody\Bridge\Model\Export\OrderEntry::STATUS_NOT_PUSH,
+            ]);
+
+            try {
+                $this->orderEntryRepository->save($orderEntryModel);
+            } catch (\Exception $e) {
+                $this->logger->critical($e);
+            }
+        }
+    }
+
     public function updateOrders(array $orderIds)
     {
         $ordersData = $this->entityOrderEntryRepository->getOrders($orderIds);
-        
+
         foreach ($ordersData as $orderData) {
             $this->updateOrder($orderData);
         }
     }
-    
+
     private function prepareOrderData(\Magento\Sales\Model\Order $order) : array
     {
         return [
@@ -75,12 +106,12 @@ class OrderService implements OrderServiceInterface
             'products' => $this->prepareOrderItemsData($order),
         ];
     }
-    
+
     private function prepareOrderAddressData(\Magento\Sales\Model\Order $order) : array
     {
         /* @var \Magento\Sales\Model\Order\Address $address */
         $address = $order->getShippingAddress();
-        
+
         return [
             'name' => $address->getData('firstname'),
             'surname' => $address->getData('lastname'),
@@ -96,11 +127,11 @@ class OrderService implements OrderServiceInterface
             'email' => $order->getData('customer_email'),
         ];
     }
-    
+
     private function prepareOrderItemsData(\Magento\Sales\Model\Order $order) : array
     {
         $items = [];
-        
+
         /* @var \Magento\Sales\Model\Order\Item $item */
         foreach ($order->getAllVisibleItems() as $item) {
             $items[] = [
@@ -113,25 +144,25 @@ class OrderService implements OrderServiceInterface
                 'tax' => $item->getData('tax_percent'),
             ];
         }
-        
+
         return $items;
     }
-    
+
     private function prepareOrderPhoneNumber(\Magento\Sales\Model\Order\Address $address) : string
     {
         $telephone = $address->getData('telephone');
         $landlineTelephone = $address->getData('landline_telephone');
-        
+
         return $telephone ? : $landlineTelephone;
     }
-    
+
     private function updateOrder(array $orderData)
     {
         $orderId = (int) $orderData['order_id'];
-    
+
         $availableStatuses = $this->orderConfig->getStatuses();
         $status = null;
-    
+
         if (true !== isset($availableStatuses[$orderData['status']])) {
             if ('processing' === $orderData['state']) {
                 $status = 'processing';
@@ -141,18 +172,18 @@ class OrderService implements OrderServiceInterface
         } else {
             $status = $orderData['status'];
         }
-    
+
         /** @var \Magento\Sales\Model\Order $order */
         $order = $this->orderRepository->get($orderId);
-    
+
         $order->setData('state', $orderData['state']);
         $order->setData('status', $status);
-    
+
         if (false === empty($orderData['tracking_number'])) {
             $this->shipmentService->createOrUpdate($order, $orderData);
         }
-    
+
         $this->orderRepository->save($order);
     }
-    
+
 }
